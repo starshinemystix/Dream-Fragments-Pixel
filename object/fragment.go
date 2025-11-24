ba9d474a950489a9d15cc29b20fb4ab13e37c86b
StarShineMystix <starshinemystix@gmail.com>
2025-11-24 22:43:28 +0000

    Add files via upload

---
+++ object/fragment.go
@@ -0,0 +1,46 @@
+components {
+  id: "fragment1"
+  component: "/object/fragment.script"
+}
+components {
+  id: "pickup"
+  component: "/assets/pickup.sound"
+}
+embedded_components {
+  id: "fragment"
+  type: "sprite"
+  data: "default_animation: \"idle\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/fragment.atlas\"\n"
+  "}\n"
+  ""
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
+  "friction: 0.0\n"
+  "restitution: 0.0\n"
+  "group: \"fragments\"\n"
+  "mask: \"player\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: -14.0\n"
+  "      y: 108.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 750.0\n"
+  "  data: 875.0\n"
+  "  data: 10.0\n"
+  "}\n"
+  ""
+}
